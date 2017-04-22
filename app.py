875161c60d0193d3f345152adbcb53071a902dd6
Thomas F <taforres@asu.edu>
2017-04-22 17:21:28 -0600

    Add stubs for intents

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from flask_ask import Ask, statement
+from flask_ask import Ask, statement, question ,convert_errors
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-@ask.intent('HelloIntent')
-def hello(firstname):
-    speech_text = "Hello %s" % firstname
+
+@ask.intent('helloworld')
+def hello(mood_level):
+    speech_text = "Hello %s! This is working" % firstname
     return statement(speech_text).simple_card('Hello', speech_text)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ intents.py
@@ -0,0 +1,28 @@
+from flask_ask import Ask, statement, question ,convert_errors
+
+def mood_intent(mood_level):
+    pass
+
+def productivity_intent(productivity_level):
+    pass
+
+def sleep_intent(sleep_level):
+    pass
+
+def excercise_intent(answer):
+    pass
+
+def entertainment_intent(answer):
+    pass
+
+def schoolwork_intent(answer):
+    pass
+
+def work_intent(answer):
+    pass
+
+def socialize_intent(answer):
+    pass
+
+def conflict_intent(answer):
+    pass
