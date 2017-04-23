184de4a87304c6d1fbbd41890b2e41e297ed6d76
Michelle Capriles-Escobedo <caprilem@gmail.com>
2017-04-23 08:25:02 -0600

    Data Array Creator
    Javascript file that creates data array

---
+++ website/dataArrayCreator.js
@@ -0,0 +1,9 @@
+ <script src="http://jquery-csv.googlecode.com/git/src/jquery.csv.js"></script>
+
+ var file = new File([""], "RealTalkSessions.csv");
+ var reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = function(event){
+        var csv = event.target.result;
+        var data = $.csv.toArrays(csv);
+      }
